5446b683489d0cdd167a450eb0a957d3f57e15cf
Stanislav Radyk <stasradik@gmail.com>
2023-09-11 13:28:49 +0100

    Initial commit

---
+++ functions.py
@@ -0,0 +1,21 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath='todos.txt'):
+    """Read a text file and return list of
+    to-do items
+    """
+    with open(filepath, "r") as file_local:
+        """Write to-do items in a list"""
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath='todos.txt'):
+    with open(filepath, "w") as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
